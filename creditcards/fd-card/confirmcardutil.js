1213496f69cf1613d70bdd603352e13411486851
kunal-adobe <kunalj.adobe@gmail.com>
2024-08-30 10:02:16 +0530

    lint fix

--- creditcards/fd-card/confirmcardutil.js
+++ creditcards/fd-card/confirmcardutil.js
@@ -7,11 +7,11 @@ import { getUrlParamCaseInsensitive } from '../../common/formutils.js';
  */
 const confirmCardClickHandler = (globals) => {
   const { addressDetails, employeeAssistance } = globals.form.fdBasedCreditCardWizard.basicDetails.reviewDetailsView;
-  const {aadharBiometricVerification} = globals.form.selectKYCOptionsPanel.selectKYCMethodOption1;
+  const { aadharBiometricVerification } = globals.form.selectKYCOptionsPanel.selectKYCMethodOption1;
   const inPersonBioKYC = getUrlParamCaseInsensitive('InpersonBioKYC');
-  if ((addressDetails.mailingAddressToggle._data.$_value === 'off' || inPersonBioKYC.toLowerCase() === 'yes') && 
-  employeeAssistance.inPersonBioKYCPanel.inPersonBioKYCOptions._data.$_value === '0'){
-    globals.functions.setProperty(aadharBiometricVerification, {value: '0'});
+  if ((addressDetails.mailingAddressToggle._data.$_value === 'off' || inPersonBioKYC.toLowerCase() === 'yes')
+  && employeeAssistance.inPersonBioKYCPanel.inPersonBioKYCOptions._data.$_value === '0') {
+    globals.functions.setProperty(aadharBiometricVerification, { value: '0' });
   }
 };
 
