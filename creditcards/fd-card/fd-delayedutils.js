24a06783bb381e849c9bb597b332009fd6304a90
kunal-adobe <kunalj.adobe@gmail.com>
2024-09-18 18:29:31 +0530

    reomved cc code

--- creditcards/fd-card/fd-delayedutils.js
+++ creditcards/fd-card/fd-delayedutils.js
@@ -22,62 +22,7 @@ const fdCardBoardingSuccess = async (data, stateInfoData) => {
   const {
     executeInterfaceReqObj, aadharOtpValData, finalDapRequest, finalDapResponse,
   } = data;
-  const journeyName = executeInterfaceReqObj?.requestString?.journeyFlag;
-  const addressEditFlag = executeInterfaceReqObj?.requestString?.addressEditFlag;
-  const { applicationNumber, vkycUrl } = finalDapResponse;
-  const { CURRENT_FORM_CONTEXT: currentFormContext } = (await import('../../common/constants.js'));
-  currentFormContext.VKYC_URL = vkycUrl;
-  // const { result: { mobileValid } } = aadharOtpValData;
-  const mobileValid = aadharOtpValData?.result?.mobileValid;
-  const resultPanel = document.getElementsByName('resultPanel')?.[0];
-  const successPanel = document.getElementsByName('successResultPanel')?.[0];
-  resultPanel.setAttribute('data-visible', true);
-  successPanel.setAttribute('data-visible', true);
-  // setArnNumberInResult(applicationNumber);
 
-  const vkycProceedButton = document.querySelector('.field-vkycproceedbutton ');
-  const offerLink = document.querySelector('.field-offerslink');
-  const vkycConfirmText = document.querySelector('.field-vkycconfirmationtext');
-  // const filler4Val = finalDapRequest?.requestString?.VKYCConsent?.split(/[0-9]/g)?.[0];
-  // const mobileMatch = !(filler4Val === 'NVKYC');
-  const mobileMatch = !(mobileValid === 'n'); // (mobileValid === 'n') - unMatched - this should be the condition which has to be finalDap - need to verify.
-  const kycStatus = (finalDapRequest.requestString.biometricStatus);
-  const vkycCameraConfirmation = document.querySelector(`[name= ${'vkycCameraConfirmation'}]`);
-  const vkycCameraPannelInstruction = document.querySelector('.field-cameraconfirmationpanelinstruction');
-
-  if (journeyName === 'ETB') {
-    if (addressEditFlag === 'N') {
-      vkycProceedButton.setAttribute('data-visible', false);
-      vkycConfirmText.setAttribute('data-visible', false);
-      offerLink.setAttribute('data-visible', true);
-    } else if (kycStatus === 'OVD') {
-      vkycProceedButton.setAttribute('data-visible', false);
-      vkycConfirmText.setAttribute('data-visible', true);
-      offerLink.setAttribute('data-visible', false); // Adjusted assumption for offerLink
-    } else if (mobileMatch && kycStatus === 'aadhaar' && addressEditFlag === 'Y') {
-      vkycProceedButton.setAttribute('data-visible', false);
-      vkycConfirmText.setAttribute('data-visible', false);
-      offerLink.setAttribute('data-visible', true);
-    } else {
-      vkycProceedButton.setAttribute('data-visible', true);
-      currentFormContext.isVideoKyc = true;
-      vkycConfirmText.setAttribute('data-visible', true);
-      offerLink.setAttribute('data-visible', false);
-    }
-  }
-  if (journeyName === 'NTB' && (kycStatus === 'aadhaar')) {
-    vkycCameraConfirmation.setAttribute('data-visible', true);
-    vkycCameraPannelInstruction.setAttribute('data-visible', true);
-    vkycProceedButton.setAttribute('data-visible', true);
-    currentFormContext.isVideoKyc = true;
-  }
-  currentFormContext.action = 'confirmation';
-  currentFormContext.pageGotRedirected = true;
-  // temporarly commented and it will be enabled after analytics merge.
-  // Promise.resolve(sendPageloadEvent('CONFIRMATION_JOURNEY_STATE', stateInfoData));
-  const mobileNumber = stateInfoData.form.login.registeredMobileNumber;
-  const leadProfileId = stateInfoData.leadProifileId;
-  const journeyId = stateInfoData.currentFormContext.journeyID;
   invokeJourneyDropOffUpdate('CUSTOMER_ONBOARDING_COMPLETED', mobileNumber, leadProfileId, journeyId, stateInfoData);
 };
 
@@ -114,16 +59,16 @@ const invokeJourneyDropOffByParam = async (mobileNumber, leadProfileId, journeyI
  * @name finalDapFetchRes - recursive async action call maker untill it reaches the finaldap response.
  * @returns {void} error method or succes method based on the criteria of finalDapResponse reach or max limit reach.
  */
-const finalDap = async () => {
+const finalDapFetchRes = async () => {
   const { finalDapConst } = delayedUtilState;
   const eventHandler = {
     successMethod: (data) => {
       console.log(data);
-      // const {
-      //   currentFormContext: {
-      //     executeInterfaceReqObj, finalDapRequest, finalDapResponse,
-      //   }, aadhaar_otp_val_data: aadharOtpValData,
-      // } = JSON.parse(data.stateInfo);
+      const {
+        currentFormContext: {
+          executeInterfaceRequest, finalDapRequest, finalDapResponse,
+        }, aadhaar_otp_val_data: aadharOtpValData,
+      } = JSON.parse(data.stateInfo);
       hideLoaderGif();
       // successPannelMethod({
       //   executeInterfaceReqObj, aadharOtpValData, finalDapRequest, finalDapResponse,
@@ -154,7 +99,7 @@ const finalDap = async () => {
     if (errorCase) {
       return eventHandler.errorMethod(error, JSON.parse(stateInfoData));
     }
-    return setTimeout(() => finalDap(), 5000);
+    return setTimeout(() => finalDapFetchRes(), 5000);
   }
 };
 
@@ -168,7 +113,7 @@ const pageRedirected = () => {
      */
     setTimeout(() => {
       displayLoader();
-      finalDap();
+      finalDapFetchRes();
     }, 2000);
   }
 };
