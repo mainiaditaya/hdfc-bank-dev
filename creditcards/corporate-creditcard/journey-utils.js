bd7222b7735598f4a265982633ad06545231f5ab
mainiaditaya <mainiaditaya@gmail.com>
2024-09-26 16:12:52 +0530

    Update journey-utils.js

--- creditcards/corporate-creditcard/journey-utils.js
+++ creditcards/corporate-creditcard/journey-utils.js
@@ -113,6 +113,10 @@ const invokeJourneyDropOffUpdate = async (state, mobileNumber, leadProfileId, jo
       leadProfile: {
         mobileNumber,
         leadProfileId: leadProfileId?.toString(),
+         profile: {
+          dob: currentFormContext.dob,
+          fullName: currentFormContext.fullName,
+        },
       },
       formData: {
         channel: CHANNEL,
