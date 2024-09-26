10a7616043abcb1666070b60c953e5f24d0effb5
Deep Prakash Dewanji <deepprakashdewanji@labusers-MacBook-Pro-9.local>
2024-09-26 21:05:53 +0530

    fix: updated constants.js submitUrl..

--- blocks/form/constant.js
+++ blocks/form/constant.js
@@ -7,8 +7,12 @@ export const defaultErrorMessages = {
   maxItems: 'Specify a number of items equal to or less than $0.',
   minItems: 'Specify a number of items equal to or greater than $0.',
 };
-
 let submitBaseUrl = '';
+
+if(window.location.hostname.includes("aem.live") || window.location.hostname.includes("aem.page")){
+   submitBaseUrl = 'https://hdfc-dev-04.adobecqms.net';
+}
+
 
 export function setSubmitBaseUrl(url) {
   submitBaseUrl = url;
