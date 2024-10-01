797855a022ef1c4f8e77271966a52cdbcbd78a46
bazilMuralidharanDev <bazil.m@dizitalpods.com>
2024-10-01 10:47:37 +0530

    default open accordian is required

--- blocks/form/components/accordion-semi.js
+++ blocks/form/components/accordion-semi.js
@@ -5,7 +5,7 @@ export default function accordionLayout(panel) {
   const panelBody = panel.querySelector('.panel-wrapper:nth-child(3)');
   const legend = panel?.querySelector('.panel-wrapper .hdfc-accordian-legend button');
   // eslint-disable-next-line no-unused-expressions
-  panel?.id === 'panelcontainer-bf227dfd9e' ? panelBody.classList.toggle('accordion-collapse') : null;
+  // panel?.id === 'panelcontainer-bf227dfd9e' ? panelBody.classList.toggle('accordion-collapse') : null;
   legend?.addEventListener('click', () => {
     panelBody.classList.toggle('accordion-collapse');
   });
