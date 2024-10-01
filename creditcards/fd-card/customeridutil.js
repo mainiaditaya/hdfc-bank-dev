10bccc3a970acffd377e7d297f033c504c9c547d
kunal-adobe <kunalj.adobe@gmail.com>
2024-10-01 15:43:50 +0530

    updated date format

--- creditcards/fd-card/customeridutil.js
+++ creditcards/fd-card/customeridutil.js
@@ -2,24 +2,21 @@
 import { CURRENT_FORM_CONTEXT } from '../../common/constants.js';
 import { FD_ENDPOINTS } from './constant.js';
 import { fetchJsonResponse, fetchRecursiveResponse } from '../../common/makeRestAPI.js';
-import { urlPath } from '../../common/formutils.js';
+import { dateFormat, urlPath } from '../../common/formutils.js';
 
 const SELECTED_CUSTOMER_ID = {};
 let selectedCustIndex = -1;
 
-const createPayload = (mobileNumber, panNumber, dateOfBirth, jwtToken) => {
-  const payload = {
-    requestString: {
-      mobileNumber,
-      dateOfBirth: dateOfBirth || '',
-      panNumber: panNumber ? panNumber.replace(/\s+/g, '') : '',
-      journeyID: CURRENT_FORM_CONTEXT.journeyID,
-      journeyName: CURRENT_FORM_CONTEXT.journeyName,
-      jwtToken,
-    },
-  };
-  return payload;
-};
+const createPayload = (mobileNumber, panNumber, dob, jwtToken) => ({
+  requestString: {
+    mobileNumber,
+    dateOfBirth: dob ? dateFormat(dob, 'YYYYMMDD') : '',
+    panNumber: panNumber ? panNumber?.replace(/\s+/g, '') : '',
+    journeyID: CURRENT_FORM_CONTEXT.journeyID,
+    journeyName: CURRENT_FORM_CONTEXT.journeyName,
+    jwtToken,
+  },
+});
 
 /**
  * Fetches the customer ID
