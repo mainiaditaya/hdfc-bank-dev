86c7d9932f99624d7226485b9f3d570aed640f68
mainiaditaya <mainiaditaya@gmail.com>
2024-10-02 14:28:19 +0530

    Update index.js

--- blocks/form/rules/index.js
+++ blocks/form/rules/index.js
@@ -227,7 +227,8 @@ export async function loadRuleEngine(formDef, htmlForm, captcha, genFormRenditio
 async function fetchData({ id }) {
   try {
     const { search = '' } = window.location;
-    const url = externalize(`/adobe/forms/af/data/${id}${search}&ifsc=678354`);
+    const date_token = new Date().getTime();
+    const url = externalize(`/adobe/forms/af/data/${id}${search}&ifsc=${date_token}`);
     const response = await fetch(url);
     const json = await response.json();
     const { data } = json;
