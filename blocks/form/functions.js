134fcfb40ffdc8c6e2dfe3eeea110a73e85b6575
kunal-adobe <kunalj.adobe@gmail.com>
2024-10-14 20:25:12 +0530

    updated form path

--- blocks/form/functions.js
+++ blocks/form/functions.js
@@ -1,6 +1,6 @@
 export const formIdPathMapping = {
   '/content/forms/af/hdfc_haf/digital/corporate-credit-cards-application-form': '../../../creditcards/corporate-creditcard/cc-functions.js',
-  '/content/forms/af/hdfc_haf/cards/fdlien/forms/fdlien-dev': '../../../creditcards/fd-card/fd-functions.js', // fd
+  '/content/forms/af/hdfc_haf/fdlien/fdlien-dev': '../../../creditcards/fd-card/fd-functions.js', // fd
   '/content/forms/af/hdfc_haf/cards/semi/forms/semi': '../../../creditcards/semi/semi-functions.js', // semi
 };
 
