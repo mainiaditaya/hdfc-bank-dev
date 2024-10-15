0f49524f1b24209f9f6570482c6220d656dfea8c
reddymanojkumar653 <reddymanojkumar653@gmail.com>
2024-10-15 16:13:29 +0530

    updated url for testing

--- blocks/form/functions.js
+++ blocks/form/functions.js
@@ -3,7 +3,7 @@ export const formIdPathMapping = {
   // cc
   // '/content/forms/af/hdfc_haf/cards/fdlien/forms/fdlien-dev': '../../../common/functions.js', // fd
   '/content/forms/af/hdfc_haf/liabilities/nre-nro/forms/account-opening-nre-nro': '../../../liabilities/nre-Nro/nre-nro.js', // nre-Nro
-  '/content/forms/af/hdfc_haf/liabilities/nre-nro/forms/account-opening-nre-nro-nomineeno': '../../../liabilities/nre-Nro/nre-nro.js', // nre-Nro
+  '/content/forms/af/hdfc_haf/liabilities/nre-nro/forms/account-opening-nre-nro2test': '../../../liabilities/nre-Nro/nre-nro.js', // nre-Nro
 };
 
 
