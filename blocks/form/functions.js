a423675861373412a0c6b665648ba2d34430362e
mainiaditaya <mainiaditaya@gmail.com>
2024-10-18 16:29:03 +0530

    Update functions.js

--- blocks/form/functions.js
+++ blocks/form/functions.js
@@ -1,7 +1,7 @@
 export const formIdPathMapping = {
   '/content/forms/af/hdfc_haf/cards/corporatecreditcard/uat/hdfc': '../../../creditcards/corporate-creditcard/cc-functions.js', // cc
   '/content/forms/af/hdfc_haf/cards/fdlien/forms/fdlien-dev': '../../../creditcards/fd-card/fd-functions.js', // fd
-  '/content/forms/af/hdfc_haf/cards/semi/forms/semi': '../../../creditcards/semi/semi-functions.js', // semi
+  '/content/forms/af/hdfc_haf/digital/semi': '../../../creditcards/semi/semi-functions.js', // semi
 };
 
 export default function getCustomFunctionPath(id) {
