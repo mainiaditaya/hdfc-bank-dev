dd2e8360e5a65f3fee0e2f79fac503ca9ea01eba
bazilMuralidharanDev <bazil.m@dizitalpods.com>
2024-10-23 12:07:06 +0530

    launchScript-PROD-semi added

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -22,7 +22,7 @@ const FORM_CONSTANT = [
     urlKey: ['semi', 'smart-emi', 'smart emi', 'smartemi'],
     launchScript: {
       dev: 'https://assets.adobedtm.com/80673311e435/029b16140ccd/launch-94203efd95a9-staging.min.js',
-      prod: 'https://assets.adobedtm.com/80673311e435/029b16140ccd/launch-94203efd95a9-staging.js',
+      prod: 'https://assets.adobedtm.com/80673311e435/029b16140ccd/launch-39d52f236cd6.min.js',
       loadTime: 0,
     },
   },
