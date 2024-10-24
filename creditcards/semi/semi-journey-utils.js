a6cadb6c9dee84f4d98c8d31aac2a34940484b14
bazilMuralidharanDev <bazil.m@dizitalpods.com>
2024-10-24 12:48:05 +0530

    journeyUtils update change

--- creditcards/semi/semi-journey-utils.js
+++ creditcards/semi/semi-journey-utils.js
@@ -5,10 +5,27 @@ import { fetchJsonResponse } from '../../common/makeRestAPI.js';
 
 /* temproraily added this journey utils for SEMI , journey utils common file has to be changed to generic */
 const CHANNEL = 'ADOBE_WEBFORMS';
-
+const isNodeEnv = typeof process !== 'undefined' && process.versions && process.versions.node;
 const {
   CURRENT_FORM_CONTEXT: currentFormContext,
 } = CONSTANT;
+
+/**
+ * For Web returing currentFormContext as defined in variable
+ * Ideally every custom function should be pure function, i.e it should not have any side effect
+ * As per current implementation `currentFormContext` is a state outside of the function,
+ * so for Flow we have did special handling by storing strigified value in `globals.form.runtime.currentFormContext`
+ *
+ * @param {scope} globals
+ * @returns
+ */
+const getCurrentFormContext = (globals) => {
+  if (isNodeEnv) {
+    return JSON.parse(globals.form.runtime.currentFormContext.$value || '{}');
+  }
+  return currentFormContext;
+};
+
 /**
    * @name invokeJourneyDropOff to log on success and error call backs of api calls
    * @param {state} state
@@ -52,7 +69,11 @@ const invokeJourneyDropOff = async (state, mobileNumber, globals) => {
      * @return {PROMISE}
      */
 const invokeJourneyDropOffUpdate = async (state, mobileNumber, leadProfileId, journeyId, globals) => {
-  const sanitizedFormData = santizedFormDataWithContext(globals, currentFormContext);
+  const formContext = getCurrentFormContext(globals);
+  if (state === 'CUSTOMER_ONBOARDING_COMPLETE') {
+    formContext.loanReferenceNumber = journeyId?.loanNbr;
+  }
+  const sanitizedFormData = santizedFormDataWithContext(globals, formContext);
   const journeyJSONObj = {
     RequestPayload: {
       userAgent: (typeof window !== 'undefined') ? window.navigator.userAgent : '',
@@ -63,7 +84,7 @@ const invokeJourneyDropOffUpdate = async (state, mobileNumber, leadProfileId, jo
       formData: {
         channel: CHANNEL,
         journeyName: globals.form.runtime.journeyName.$value,
-        journeyID: journeyId,
+        journeyID: globals.form.runtime.journeyId.$value,
         journeyStateInfo: [
           {
             state,
