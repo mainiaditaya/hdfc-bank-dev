efa28f28e6359ccdbde6dd7cc2270691bec3bea4
findvinu <vinodyadavhhh4@gmail.com>
2024-11-04 02:58:46 -0800

    accordian functionality

--- blocks/form/components/accordion.js
+++ blocks/form/components/accordion.js
@@ -1,16 +1,38 @@
 /**
  * Represents a layout manager for creating accordion-style UI components.
  */
+
+let currentlyOpenPanel = null;
 export default function accordionLayout(panel) {
-  const legend = panel?.querySelector('.hdfc-accordion-legend') || panel?.querySelector('legend');
+  const legend = panel?.querySelector('legend');
   legend?.classList.add('hdfc-accordion-legend');
+
   legend?.addEventListener('click', () => {
+    if (currentlyOpenPanel && currentlyOpenPanel !== panel) {
+      const currentlyOpenLegend = currentlyOpenPanel.querySelector('legend');
+      currentlyOpenLegend.classList.remove('accordion-collapse');
+
+      Array.from(currentlyOpenPanel.children).forEach((childElement) => {
+        if (childElement !== currentlyOpenLegend) {
+          childElement.style.display = 'none';
+        }
+      });
+    }
+
     legend.classList.toggle('accordion-collapse');
+
     Array.from(panel.children).forEach((childElement) => {
       if (childElement !== legend) {
         childElement.style.display = (childElement.style.display === 'none') ? '' : 'none';
       }
     });
+
+    currentlyOpenPanel = legend.classList.contains('accordion-collapse') ? panel : null;
   });
+
+  if (!currentlyOpenPanel) {
+    legend.click();
+  }
+
   return panel;
 }
