111d413b77574ad78c360cb22b5cf15ccc93a82a
jdhall <jdhall@adobe.com>
2024-11-13 19:19:09 +0530

    Lint issues fix

--- liabilities/nre-Nro/pageloadworker.js
+++ liabilities/nre-Nro/pageloadworker.js
@@ -1,28 +1,28 @@
-if(window && document){
-    let thanks = document.querySelector('[name="thankYouPanel"]');
-    let errorPanel = document.querySelector('[name="itsNotYouPanel"]');
-    const mutationCallback = (mutationsList) => {
-        for (const mutation of mutationsList) {
-        let dataVisibileValue = mutation.target.getAttribute("data-visible");
-        let currTarget = mutation.target.name;
-          if(dataVisibileValue && dataVisibileValue === 'true'){
-            switch(currTarget){
-               case 'thankYouPanel':
-                document.body.classList.add('nreThankYouPage');
-                document.body.classList.remove('errorPageBody');
-                break;
-               case 'itsNotYouPanel':
-                document.body.classList.add('errorPageBody');
-                document.body.classList.remove('nreThankYouPage');
-                break;
-               default:
-                break;
-            }
-            
-          }
+if (window && document) {
+  const thanks = document.querySelector('[name="thankYouPanel"]');
+  const errorPanel = document.querySelector('[name="itsNotYouPanel"]');
+  const mutationCallback = (mutationsList) => {
+    // eslint-disable-next-line
+    for (const mutation of mutationsList) {
+      const dataVisibileValue = mutation.target.getAttribute('data-visible');
+      const currTarget = mutation.target.name;
+      if (dataVisibileValue && dataVisibileValue === 'true') {
+        switch (currTarget) {
+          case 'thankYouPanel':
+            document.body.classList.add('nreThankYouPage');
+            document.body.classList.remove('errorPageBody');
+            break;
+          case 'itsNotYouPanel':
+            document.body.classList.add('errorPageBody');
+            document.body.classList.remove('nreThankYouPage');
+            break;
+          default:
+            break;
         }
+      }
     }
-    const observer = new MutationObserver(mutationCallback);
-    observer.observe(thanks, { attributes: true });
-    observer.observe(errorPanel, { attributes: true });
+  };
+  const observer = new MutationObserver(mutationCallback);
+  observer.observe(thanks, { attributes: true });
+  observer.observe(errorPanel, { attributes: true });
 }
