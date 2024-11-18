9fe65919a776ffad66a0db3e29c6f9f9140280a3
BazilMuralidharan <92780543+BazilMuralidharan@users.noreply.github.com>
2024-11-18 14:46:19 +0530

    fdlienprodtest key added in script.js, form-functions.js & semi-md-utils,ccc-executeinterfaceutils lint fix (#868)
    Co-authored-by: bazilMuralidharanDev <bazil.m@dizitalpods.com>

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -39,9 +39,9 @@ const FORM_CONSTANT = [
   },
   {
     // FD
-    formPath: ['etb-fixed-deposit-cc', 'pvtestfdliencugtest', 'fd-lien-cug-test'],
+    formPath: ['etb-fixed-deposit-cc', 'pvtestfdliencugtest', 'fd-lien-cug-test', 'fdlienprodtest'],
     class: 'fdlien',
-    urlKey: ['fdlien', 'pvtestfdliencugtest', 'fd-lien-cug-test', 'etb-fixed-deposit-cc'],
+    urlKey: ['fdlien', 'pvtestfdliencugtest', 'fd-lien-cug-test', 'etb-fixed-deposit-cc', 'fdlienprodtest'],
     launchScript: {
       dev: 'https://assets.adobedtm.com/80673311e435/029b16140ccd/launch-a47f215bcdb9-development.min.js',
       prod: 'https://assets.adobedtm.com/80673311e435/029b16140ccd/launch-a47f215bcdb9-development.min.js',
