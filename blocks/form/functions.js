83cb09027cfa2b37a566095bbe0bfa02aa69209a
srivastava21ashish <srivastava21ashish@gmail.com>
2025-01-09 19:21:14 +0530

    Update functions.js

--- blocks/form/functions.js
+++ blocks/form/functions.js
@@ -10,6 +10,7 @@ export const formIdPathMapping = {
   '/content/forms/af/hdfc_haf/digital/pvtestfdliencugtest': '../../../creditcards/fd-card/fd-functions.js', // fd
   '/content/forms/af/hdfc_haf/digital/fd-lien-cug': '../../../creditcards/fd-card/fd-functions.js', // fd
   '/content/forms/af/hdfc_haf/digital/fdlienprodtest': '../../../creditcards/fd-card/fd-functions.js', // fd form
+  '/content/forms/af/hdfc_haf/digital/account-opening-nre-nro': '../../../liabilities/nre-Nro/nre-nro.js', 
 };
 
 export default function getCustomFunctionPath(id) {
