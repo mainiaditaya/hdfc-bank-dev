24be544b52b8754c79c521d5b2b758615325bb12
bazilMuralidharanDev <bazil.m@dizitalpods.com>
2025-01-24 11:37:38 +0530

    blocks-function-lint fix

--- blocks/form/functions.js
+++ blocks/form/functions.js
@@ -9,9 +9,9 @@ export const formIdPathMapping = {
   '/content/forms/af/hdfc_haf/loan-against-assets/smartemi/smartemi': '../../../creditcards/semi/semi-functions.js',
   '/content/forms/af/hdfc_haf/digital/pvtestfdliencugtest': '../../../creditcards/fd-card/fd-functions.js', // fd
   '/content/forms/af/hdfc_haf/digital/fd-lien-cug': '../../../creditcards/fd-card/fd-functions.js', // fd
-  '/content/forms/af/hdfc_haf/digital/corporate-credit-cards-application-cug': '../../../creditcards/corporate-creditcard/cc-functions.js'// ccc cug
+  '/content/forms/af/hdfc_haf/digital/corporate-credit-cards-application-cug': '../../../creditcards/corporate-creditcard/cc-functions.js', // ccc cug
   '/content/forms/af/hdfc_haf/digital/fdlienprodtest': '../../../creditcards/fd-card/fd-functions.js', // fd form
-  '/content/forms/af/hdfc_haf/digital/account-opening-nre-nro': '../../../liabilities/nre-Nro/nre-nro.js', 
+  '/content/forms/af/hdfc_haf/digital/account-opening-nre-nro': '../../../liabilities/nre-Nro/nre-nro.js',
 };
 
 export default function getCustomFunctionPath(id) {
